5922125696a6beca88cf8a0c936cc66019409f0c
HuyGemini <kimera004@gmail.com>
2016-02-26 17:29:06 +0700

    Added files via upload

---
+++ Array.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#define MAXLEN 100
+
+struct array {
+    float a[MAXLEN];
+    int count;
+};
+
---
+++ CheckArray.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Array.h"
+
+
+int main(){
+    printf("Hello");
+    return 0;
+}
